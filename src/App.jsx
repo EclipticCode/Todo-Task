1ed0d036fd84182a667a2b5d9c6a9460bf07c11b
daylightcoderj6 <jananibharathi6@gmail.com>
2024-03-29 09:46:29 +0530

    added input fields and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import './App.css'
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <h2 className='heading'>My Todo</h2>
+       <div className="row">
+         <div className="col-sm-5 col-md-5">
+           <input type="text" className="form-control text-muted" placeholder="Todo Name" aria-label="Username" aria-describedby="addon-wrapping" />
+         </div>
+         <div className="col-sm-5 col-md-5">
+           <input type="text" className="form-control text-muted" placeholder="Todo Description" aria-label="Username" aria-describedby="addon-wrapping"/>
+         </div>
+         <div className="col-sm-2 col-md-2">
+           <button type="button" className="btn">Add Todo</button>
+         </div>
+       </div>
+   </div>
+
+ 
   )
 }
 
