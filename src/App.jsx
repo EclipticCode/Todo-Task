62ffe04b1b7de8735b10e74c06ecadad978ecf09
daylightcoderj6 <jananibharathi6@gmail.com>
2024-03-30 11:34:22 +0530

    moved to seaparate components

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import Heading from './assets/components/Heading'
 import Inputs from './assets/components/Inputs'
 import Card from './assets/components/Card'
+
 const App = () => {
 
 const [name,setName] = useState("")
 const [desc,setDesc] = useState("")
 const [showCard,setShowCard] = useState(false)
+const [boxes,setBoxes] = useState([])
 
 const todoName = (e) => {
    setName(e.target.value)
+   e.preventDefault()
 }
 const todoDesc = (e) => {
    setDesc(e.target.value)
 }
-const addTodoButton = () =>{
+const addTodoButton = () => {
   setShowCard(true)
+  setBoxes([...boxes,{name,desc}])
+  // console.log(boxes)
+  setName("")
+  setDesc("")
 }
+// console.log(boxes)
+
   return (
     <div>
        <Heading/>
@@ -26,10 +35,23 @@ const addTodoButton = () =>{
        desc={desc}
        onDescChange={todoDesc}
        addTodoButton={addTodoButton}/>
-       <Card
-       name={name}
-       desc={desc}
-       showCard={showCard}/>
+       <div className="container">
+       <div className='row'>
+       { boxes.map((box,index)=>(
+        <div className="col-sm-12 col-md-6 col-lg-4" key={index}>
+          <Card
+          name={box.name}
+          desc={box.desc}
+          showCard={showCard}
+          />
+        </div>
+        ))
+       }
+        
+
+        
+       </div>
+       </div>
    </div>
 
  
--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 import "./Card.css"
 
 const Card = ({name,desc,showCard}) => {
+
   return (
    <div>
      { showCard ? (
-      <div className='container'>
-      <div className="row">
-      <div className="col-sm-3">
       <div className='card'>
       <div className="card-body">
       <b>Name:</b> {name}
@@ -27,9 +25,6 @@ const Card = ({name,desc,showCard}) => {
       <button type="button" className="btn btn-danger">Delete</button>
       </div>
       </div>
-      </div>
-    </div>
-  </div>
      ) :
      <div></div>}
    </div>
