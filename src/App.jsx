3e80161096d1f64bd30cece48b58d36a0568bbf1
daylightcoderj6 <jananibharathi6@gmail.com>
2024-04-04 12:52:13 +0530

    added readme.md

--- src/App.jsx
+++ src/App.jsx
@@ -6,82 +6,82 @@ import Card from './assets/components/Card'
 
 const App = () => {
 
-const [name,setName] = useState("")
-const [desc,setDesc] = useState("")
-const [showCard,setShowCard] = useState(false)
-const [boxes,setBoxes] = useState([])
-const [filter,setFilter] = useState('All')
+  const [name, setName] = useState("")
+  const [desc, setDesc] = useState("")
+  const [showCard, setShowCard] = useState(false)
+  const [boxes, setBoxes] = useState([])
+  const [filter, setFilter] = useState('All')
 
-const todoName = (e) => {
-  setName(e.target.value)
-  e.preventDefault()
-}
-const todoDesc = (e) => {
-  setDesc(e.target.value)
-}
-const addTodoButton = () => {
-  setShowCard(true)
-  setBoxes([...boxes,{name,desc,status:"Not Completed"}])
-  setName("")
-  setDesc("")
-}
-const handleDelete = (index) => {
-  const updatedBoxes = [...boxes]
-  updatedBoxes.splice(index,1)
-  setBoxes(updatedBoxes)
-}
+  const todoName = (e) => {
+    setName(e.target.value)
+    e.preventDefault()
+  }
+  const todoDesc = (e) => {
+    setDesc(e.target.value)
+  }
+  const addTodoButton = () => {
+    setShowCard(true)
+    setBoxes([...boxes, { name, desc, status: "Not Completed" }])
+    setName("")
+    setDesc("")
+  }
+  const handleDelete = (index) => {
+    const updatedBoxes = [...boxes]
+    updatedBoxes.splice(index, 1)
+    setBoxes(updatedBoxes)
+  }
 
-const editButton = (name,desc) => {
-  setName(name)
-  setDesc(desc)
-}
-const handleFilterChange = (e) => {
-  setFilter(e.target.value);
-};
-const filteredBoxes = boxes.filter((box)=>{
-  if (filter === "All"){
-    return true;
+  const editButton = (name, desc) => {
+    setName(name)
+    setDesc(desc)
   }
-  else if (filter === "Completed"){
-    return box.status === true;
-  }
-  else if (filter === "Not Completed"){
-    return box.status === true;
-  }
-})
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  };
+  const filteredBoxes = boxes.filter((box) => {
+    if (filter === "All") {
+      return true;
+    }
+    else if (filter === "Completed") {
+      return box.status === true;
+    }
+    else if (filter === "Not Completed") {
+      return box.status === true;
+    }
+  })
   return (
     <div>
-       <Heading/>
-       <Inputs name = {name}
-       onNameChange={todoName}
-       desc={desc}
-       onDescChange={todoDesc}
-       addTodoButton={addTodoButton}
-       filter={filter}
-       onFilterChange={handleFilterChange}/>
-       <div className="container">
-       <div className='row'>
-       {filteredBoxes.map((box,index)=>(
-        <div className="col-sm-12 col-md-6 col-lg-4" key={index}>
-          <Card
-          name={box.name}
-          desc={box.desc}
-          status={box.status}
-          showCard={showCard}
-          editButton={editButton}
-          deleteButton={()=>handleDelete(index)}
-          />
+      <Heading />
+      <Inputs name={name}
+        onNameChange={todoName}
+        desc={desc}
+        onDescChange={todoDesc}
+        addTodoButton={addTodoButton}
+        filter={filter}
+        onFilterChange={handleFilterChange} />
+      <div className="container">
+        <div className='row'>
+          {filteredBoxes.map((box, index) => (
+            <div className="col-sm-12 col-md-6 col-lg-4" key={index}>
+              <Card
+                name={box.name}
+                desc={box.desc}
+                status={box.status}
+                showCard={showCard}
+                editButton={editButton}
+                deleteButton={() => handleDelete(index)}
+              />
+            </div>
+          ))
+          }
+
+
+
         </div>
-        ))
-       }
-        
+      </div>
+    </div>
 
-        
-       </div>
-       </div>
-   </div>
 
- 
   )
 }
 
--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -1,42 +1,42 @@
 import React, { useState } from 'react'
 import "./Card.css"
 
-const Card = ({name,desc,showCard,editButton,deleteButton,status}) => {
-const [selectedOption, setSelectedOption] = useState(status);
-const colorOptions = { 
-  'Completed' : "lightgreen",
-  'Not Completed' : "#FE8080"
-};
-const handleSelectChange = (event) => {
-  setSelectedOption(event.target.value);
-};
- 
+const Card = ({ name, desc, showCard, editButton, deleteButton, status }) => {
+  const [selectedOption, setSelectedOption] = useState(status);
+  const colorOptions = {
+    'Completed': "lightgreen",
+    'Not Completed': "#FE8080"
+  };
+  const handleSelectChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
   return (
-   <div>
-     { showCard ? (
-      <div className='card' >
-      <div className="card-body">
-      <b>Name:</b> {name}
-      <br /> 
-      <b>Description:</b> {desc}
-      <br /> 
-      <b> Status:</b>&nbsp;
-       <span>
-        <select value={selectedOption}
-        onChange={handleSelectChange}
-        style={{ backgroundColor: colorOptions[selectedOption] }}>
-       <option value="Not Completed">Not Completed</option>
-       <option value="Completed">Completed</option>
-      </select> 
-      </span>
-      <br />
-      <br />
-      <button type="button" className="btn btn-success" onClick={()=>{editButton(name,desc)}}>Edit</button> &nbsp; &nbsp;
-      <button type="button" className="btn btn-danger" onClick={deleteButton}>Delete</button>
-      </div>
-      </div>
-     ) : <div></div> }
-   </div>
+    <div>
+      {showCard ? (
+        <div className='card' >
+          <div className="card-body">
+            <b>Name:</b> {name}
+            <br />
+            <b>Description:</b> {desc}
+            <br />
+            <b> Status:</b>&nbsp;
+            <span>
+              <select value={selectedOption}
+                onChange={handleSelectChange}
+                style={{ backgroundColor: colorOptions[selectedOption] }}>
+                <option value="Not Completed">Not Completed</option>
+                <option value="Completed">Completed</option>
+              </select>
+            </span>
+            <br />
+            <br />
+            <button type="button" className="btn btn-success" onClick={() => { editButton(name, desc) }}>Edit</button> &nbsp; &nbsp;
+            <button type="button" className="btn btn-danger" onClick={deleteButton}>Delete</button>
+          </div>
+        </div>
+      ) : <div></div>}
+    </div>
   )
 }
 
